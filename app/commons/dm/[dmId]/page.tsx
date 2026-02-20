9ce8e24b76fb1f98aad059d0304a30c4e897ec3c
NorthernAmericana <brooksjack175@gmail.com>
2026-02-20 16:39:05 -0600

    Add DM draw pad with local/server draft sync and send flow

--- app/commons/dm/[dmId]/page.tsx
+++ app/commons/dm/[dmId]/page.tsx
@@ -36,6 +36,7 @@ export default async function PrivateDmCampfirePage({
       host={dmRoom.host}
       members={dmRoom.members}
       messages={dmRoom.messages}
+      roomId={dmId}
     />
   );
 }
